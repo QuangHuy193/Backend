f79eb488ce272e4f1ba40003c181944133fac085
QuangHuy193 <quanghuylhpq@gmail.com>
2024-10-30 20:31:24 +0700

    buoi8

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,31 +1,26 @@
 const { json } = require("express");
 const connection = require("../config/database");
+const { getAllUsers } = require("../services/CRUDServices");
 
-const getHomepage = (req, res) => {
-  res.render("home.ejs");
-  // let users = [];
-  // connection.query("select * from Users u", function (err, result, fields) {
-  //   users = result;
-  //   res.send(JSON.stringify(users));
-  // });
+//*
+const getHomepage = async (req, res) => {
+  let results = await getAllUsers();
+  return res.render("home.ejs", { listUsers: results });
 };
 
 const getUser = (req, res) => {
   return res.render("addUser.ejs");
 };
 
-const addUser = (req, res) => {
+const addUser = async (req, res) => {
   let email = req.body.email;
   let name = req.body.username;
   let city = req.body.city;
-  connection.query(
+  const [results, fields] = await connection.query(
     `insert into Users(email, name, city) values(?, ?, ? )`,
-    [email, name, city],
-    function (err, results) {
-      res.send("create user success");
-      console.log(results);
-    }
+    [email, name, city]
   );
+  return res.send("create user success");
 };
 
 module.exports = {
